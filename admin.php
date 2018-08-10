9d271faa49239fdb9ecf62d86184a7d09fe90214
Jefsky <hwj3344@hotmail.com>
2018-08-10 16:18:39 +0800

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,10 @@
+﻿<?php 
+
+	
+define('APP_NAME','.Admin');
+define('APP_PATH','Admin/');
+define('APP_DEBUG',True);
+require 'ThinkPHP/ThinkPHP.php';
+
+
+?>  
---
+++ index.php
@@ -0,0 +1,10 @@
+﻿<?php 
+
+	
+define('APP_NAME','.Index');
+define('APP_PATH','Index/');
+define('APP_DEBUG',True);
+require 'ThinkPHP/ThinkPHP.php';
+
+
+?>  
